ee530d858d6a5d2f5d048f02eb2bcc9204558311
ghanist <ghanistkumar.p@ciright.com>
2024-07-12 19:55:04 +0530

    Random note place

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ function getRandomPosition(maxWidth, maxHeight) {
 
 function App() {
   const notes = [
-    "Happy 1 Year Anniversary to US!🥰🥳🥳🫶",
+    
     "Many More Years to Go!!!!!🫰🤌",
     "You are the most beautiful thing that has ever happened to me😚",
     "I Crave for your memories every Day! You know what are my special days??? Every time when I am with you!😇😁",
     "I can bet on your love not just because you are my gonna be wifey... but I have seen your love in your eyes.💖💝",
-    "I wish for our undying relationship. With all the heartbreaks and tears, we can make it to the end. I believe in us and I love you infinitely.💗"
+    "I wish for our undying relationship. With all the heartbreaks and tears, we can make it to the end. I believe in us and I love you infinitely.💗",
+    "Happy 1 Year Anniversary to US!🥰🥳🥳🫶",
   ];
 
   const maxWidth = window.innerWidth - 300;  // Subtracting the note width
